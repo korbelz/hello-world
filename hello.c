7f43e49fe8bd829897f6a1c93afd4e3f609c1ed3
korbelz130@gmail.com <ubuntu@ubuntu.(none)>
2014-03-18 14:15:43 +0100

    verison 3, addded user input(poorly)

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	printf("Hello World \n");
-	printf("this is verison 2.0 \n");
+	char name[20];
+	printf("Please enter your name: ");
+	// Buffer overflow yourself buddy!
+	scanf("%s", name);
+	printf("Hello %s \n", name );
+	printf("this is verison 3.0 \n");
 }
